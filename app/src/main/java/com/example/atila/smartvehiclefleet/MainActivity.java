5ff23a58873f4b76cab8d40f0a40d30f64bc5f25
Atila92 <atila.sarvari@gmail.com>
2017-10-22 17:56:35 +0200

    Settings added to navigation bar

--- app/src/main/java/com/example/atila/smartvehiclefleet/MainActivity.java
+++ app/src/main/java/com/example/atila/smartvehiclefleet/MainActivity.java
@@ -132,10 +132,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_manage) {
-            // Handle the camera action
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_scan) {
 
         }
 
